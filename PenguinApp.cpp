6383a6435867024be0596964a2f2318081b5760a
Kevin Tang <klt592@cs.utexas.edu>
2013-04-13 15:07:04 -0500

    Setup base framework for game.

---
+++ PenguinApp.cpp
@@ -0,0 +1,31 @@
+#include "PenguinApp.h"
+ 
+#include "MenuState.h"
+ 
+PenguinApp::PenguinApp()
+{
+	m_pAppStateManager = 0;
+}
+ 
+PenguinApp::~PenguinApp()
+{
+	delete m_pAppStateManager;
+        delete OgreFramework::getSingletonPtr();
+}
+ 
+void PenguinApp::startGame()
+{
+	new OgreFramework();
+	if(!OgreFramework::getSingletonPtr()->initOgre("AdvancedOgreFramework", 0, 0))
+		return;
+ 
+	OgreFramework::getSingletonPtr()->m_pLog->logMessage("Game initialized!");
+ 
+	m_pAppStateManager = new AppStateManager();
+ 
+	MenuState::create(m_pAppStateManager, "MenuState");
+	//GameState::create(m_pAppStateManager, "GameState");
+        //PauseState::create(m_pAppStateManager, "PauseState");
+ 
+	m_pAppStateManager->start(m_pAppStateManager->findByName("MenuState"));
+}
---
+++ PenguinApp.h
@@ -0,0 +1,19 @@
+#ifndef PENGUINAPP_H
+#define PENGUINAPP_H
+ 
+#include "AdvancedOgreFramework.h"
+#include "AppStateManager.h"
+ 
+class PenguinApp
+{
+public:
+	PenguinApp();
+	~PenguinApp();
+ 
+	void startGame();
+ 
+private:
+	AppStateManager*	m_pAppStateManager;
+};
+ 
+#endif
